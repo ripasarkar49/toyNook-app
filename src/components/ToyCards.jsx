9599cf2e756110718fec9177c9ba9daca7f7bdee
Ripa Sarkar <ripasarkar49@gmail.com>
2025-11-16 01:51:35 +0600

    toycards design updated

--- src/components/ToyCards.jsx
+++ src/components/ToyCards.jsx
@@ -9,18 +9,13 @@ const ToyCards = ({ toys }) => {
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7">
         {toys.map((toy) => (
           <div key={toy.toyId} className="p-4 space-y-2 rounded shadow">
+            <h3 className="font-bold text-2xl mb-4">{toy.toyName}</h3>
             <img
               src={toy.pictureURL}
               alt={toy.toyName}
               className="w-full h-50 object-cover rounded"
             />
-            <h3 className="font-bold text-2xl mt-2">{toy.toyName}</h3>
-            <div className="flex justify-between ">
-              <p>Rating: {toy.rating}</p>
-              <p className="text-lg font-semibold">${toy.price}</p>
-            </div>
-            <p>AvailableQuantity: {toy.availableQuantity}</p>
-            <p className="text-sm text-gray-600 text-justify">
+             <p className="text-sm text-gray-600 text-justify">
               {" "}
               <span className=" font-bold">Product_Description: </span>
               {toy.description.length > 110
@@ -33,6 +28,12 @@ const ToyCards = ({ toys }) => {
                 View More
               </Link>
             </p>
+            <div className="flex justify-between ">
+              <p>Rating: {toy.rating}</p>
+              <p className="text-lg font-semibold">${toy.price}</p>
+            </div>
+            <p>AvailableQuantity: {toy.availableQuantity}</p>
+           
           </div>
         ))}
       </div>
